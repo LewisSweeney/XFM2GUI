60ccba4450d57d636542a65e7f1202ee367096e0
Lewis Sweeney <les35@aber.ac.uk>
2020-08-11 10:29:01 +0100

    GUIStarter class added to get past annoying JFX stuff
    TODO:
    - Allow GUI to be resizable

--- src/main/java/uk/ac/aber/les35/GUIStarter.java
+++ src/main/java/uk/ac/aber/les35/GUIStarter.java
@@ -3,6 +3,7 @@ package uk.ac.aber.les35;
 import uk.ac.aber.les35.Main;
 
 public class GUIStarter {
+    // To run GUI
     public static void main(String[] args) {
         Main.main(args);
     }
