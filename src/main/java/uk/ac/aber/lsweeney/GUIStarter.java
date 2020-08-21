4bd82a5fe2801e5104d7b01ea7b3afe9ab0f7545
les35 <les35@aber.ac.uk>
2020-08-21 12:26:41 +0100

    SERIAL BRIDGE - Now works pretty good on Windows! - Sped up Live Changes UI slowdowns by moving param changes to different threads

--- src/main/java/uk/ac/aber/lsweeney/GUIStarter.java
+++ src/main/java/uk/ac/aber/lsweeney/GUIStarter.java
@@ -1,5 +1,9 @@
 package uk.ac.aber.lsweeney;
 
+import javafx.application.Application;
+
+import static com.sun.javafx.application.LauncherImpl.launchApplication;
+
 /**
  * Class used for bypassing JFX path issues
  * Simply runs the Main class' main method
@@ -7,7 +11,8 @@ package uk.ac.aber.lsweeney;
 public class GUIStarter {
     // To run GUI
     public static void main(String[] args) {
-        Main.main(args);
+       // System.setProperty("javafx.preloader", SplashScreen.class.getCanonicalName());
+        Main.launch(Main.class, args);
     }
 
 }
--- src/main/java/uk/ac/aber/lsweeney/SplashScreen.java
+++ src/main/java/uk/ac/aber/lsweeney/SplashScreen.java
@@ -1,19 +1,50 @@
 package uk.ac.aber.lsweeney;
 
+import javafx.application.Platform;
 import javafx.application.Preloader;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class SplashScreen extends Preloader {
 
+    Stage splashStage;
+
     @Override
     public void init() throws Exception {
-        ImageView splash = new ImageView(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        Platform.runLater(() -> {
+            splashStage = new Stage();
+            splashStage.initStyle(StageStyle.TRANSPARENT);
+            splashStage.setScene(createScene());
+        });
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        splashStage.show();
+    }
+
+    private Scene createScene() {
+        ImageView splash = new ImageView(new Image(getClass().getResourceAsStream("/images/logo.png")));
+        splash.setFitHeight(200);
+        splash.setFitWidth(200);
+        VBox stageBox = new VBox(splash);
+        Scene scene = new Scene(stageBox, 200, 200);
+
+
+        return scene;
 
     }
+
+    @Override
+    public void handleApplicationNotification(PreloaderNotification arg0) {
+        if (arg0 instanceof ProgressNotification) {
+            splashStage.close();
+        }
+    }
+
+
 }
